bef0529e1935b4179a5435f584d636ef5c6bb71c
aditya agarwal <t-Aditya4.agarwal@ocltp.com>
2024-03-15 12:58:59 +0530

    Start working on App

--- App.js
+++ App.js
@@ -1,23 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// TODO: React.createElement => ReactElement(JS object) => HTMLElement(Render)
-// const heading = React.createElement("h1", {}, "Hello World 🚀");
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://media.contra.com/image/upload/c_limit,fl_lossy,w_850/qvf6zkgaymb8yl0fvcyh"
+          alt="Food Fire Logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// HTML & JSX & React all are different
-// JSX is HTML-like or XML-like syntax
-// JSX Code is transpiled by Babel before it reaches the JS Engine
-//TODO: JSX => React.createElement => ReactElement(JS object) => HTMLElement(Render)
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf"
+        alt="res-logo"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
 
-const Title = () => <h1 className="head">Hi ALL!</h1>;
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    <h1 className="heading">Namaste World!</h1>
-  </div>
-);
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
